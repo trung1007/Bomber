ff97f85467fc842c0def2b70d7d823e87f022d68
duyngo03 <21020293@vnu.edu.vn>
2022-10-20 02:46:26 +0700

    duy ngo

--- src/main/java/com/example/bomberman/Entities/Object.java
+++ src/main/java/com/example/bomberman/Entities/Object.java
@@ -60,7 +60,7 @@ public class Object {
 //            item[2] = ImageIO.read(getClass().getResourceAsStream("/sprites/powerup_speed.png"));
 //
 
-
+// alo
         }catch (IOException e){
             e.printStackTrace();
         }
