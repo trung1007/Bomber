7b8fd06ebaea3d984fd4a63916529a65cf1f2930
duyngo03 <21020293@vnu.edu.vn>
2022-10-20 18:24:50 +0700

    UI

--- src/main/java/com/example/bomberman/graphics/UI.java
+++ src/main/java/com/example/bomberman/graphics/UI.java
@@ -16,5 +16,6 @@ public class UI {
         g2.setColor(Color.red);
         g2.drawString("SizeBomb: " + gamePanel.bomb.sizeBomb, 20, 20 );
         g2.drawString("Speed: " + gamePanel.bomber.speed, 40, 40 );
+        //alo
     }
 }
